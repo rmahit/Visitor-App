f188afd9943ef78fc4612ebe145ad67e3841c81b
rmahit <mahitrbhanu@gmail.com>
2024-12-18 23:47:28 +0530

    branch created

--- app/src/main/java/com/example/visitorapp/AddActivity.java
+++ app/src/main/java/com/example/visitorapp/AddActivity.java
@@ -1,6 +1,10 @@
 package com.example.visitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,43 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddActivity extends AppCompatActivity {
 
+    EditText e1,e2,e3,e4;
+    Button b1,b2;
+    String getF,getL,getP,getW;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        e1=(EditText) findViewById(R.id.fn);
+        e2=(EditText) findViewById(R.id.ln);
+        e3=(EditText) findViewById(R.id.pp);
+        e4=(EditText) findViewById(R.id.wm);
+        b1=(Button) findViewById(R.id.sb);
+        b2=(Button) findViewById(R.id.bm);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getF=e1.getText().toString();
+                getL=e2.getText().toString();
+                getP=e3.getText().toString();
+                getW=e4.getText().toString();
+
+
+            }
         });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ob=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+
+
+
     }
 }
